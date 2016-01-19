69ce9bc7999e504f3cb1ef18065ad92514f34998
Mike Stone <michaeljacobstone@gmail.com>
2016-01-19 23:33:10 +0000

    Added songs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Firehosequiz::Application.routes.draw do
+  root 'songs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
